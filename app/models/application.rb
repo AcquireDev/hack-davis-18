fa97b9baa52a447ddf5b3ffa7b17fb39d96acddf
Jacob Bevilacqua <jacobbev@gmail.com>
2018-01-21 07:30:21 -0800

    Force updates to remove new

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+  after_update :remove_new
+
+  def remove_new
+    if self.status == new
+      self.update_attributes(status: "")
+    end
+  end
 end
